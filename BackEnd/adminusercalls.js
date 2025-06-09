a4eddaec4e16ebe5add75ac69ab2825c9803426f
samuele <sammyill91@gmail.com>
2025-06-09 17:54:54 +0200

    version 0.1.4

--- BackEnd/adminusercalls.js
+++ BackEnd/adminusercalls.js
@@ -1,6 +1,11 @@
-var bodyParser = require('body-parser');
+const crypto = require('crypto')
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+const port = 3000
 const con = require('./connector');
 const jwt=require('jsonwebtoken');
+const { path } = require('express/lib/application');
 var jsonParser = bodyParser.json();
 
 function authenticateToken(req, res, next) {
@@ -35,101 +40,92 @@ function autCheck(req,res,next,permits){
   }
   
 function initAdminUserRoutes(app) {
-    app.get('/getallusers/', authenticateToken, async (req, res) => {
-        let idcourse = req.params.idcourse;
-        let data
-        let role=req.user.roles.find(rlc=>rlc.idcourse=idcourse).idrole;
+    /**
+     * Sending back the a x(limit) amountof users
+     * starting at y(offset) user number
+     * where the first name,last name or email contains a term(searchedvalue)
+     *is will also send back if there are more user(areusersfinished)
+     */
+    app.get('/getalltheusers/:offset/:limit/:searchedvalue', authenticateToken, async (req, res) => {
+        let offset=req.params.offset;
+        let limit=req.params.limit;
+        let searchedvalue=req.params.searchedvalue
+        const searchedTerm = `%${searchedvalue}%`;
+        let areUsersFinished=false;
         try {
-            if(req.user.isAdmin===true || role===3)[data] = await con.execute(`select u.id as iduser,u.firstname,u.lastname,u.imgurl,u.status,r.id as idrole,r.name as rolename from users u
-                                                            inner join users_roles_courses urc on urc.id_user =u.id
-                                                            inner join roles r on urc.id_role =r.id
-                                                            where urc.id_course =?
-                                                            order by idrole desc`,
-                                                            [idcourse]);
-            if(req.userrole===2 || role===1)[data] = await con.execute(`select u.id as iduser,u.firstname,u.lastname,u.imgurl,u.status,r.id as idrole,r.name as rolename from users u
-                                                            inner join users_roles_courses urc on urc.id_user =u.id
-                                                            inner join roles r on urc.id_role =r.id
-                                                            where urc.id_course =? and u.status =1
-                                                            order by idrole desc`,
-                                                            [idcourse]);
-            res.json(data);
+            const [users] = await con.execute(`select * from users u 
+                                       where CONCAT(firstname, ' ', lastname, ' ', email) 
+                                       LIKE ? order by id limit ? offset ?`,
+                                        [searchedTerm,limit,offset]);
+            if(users.length<=0) areUsersFinished=true;
+            res.json({error: false,users: users,areUsersFinished:areUsersFinished});
+            console.log(users.length)
         } catch (err) {
             console.log("Getallusers Error:" + err);
             res.json({ error: true, errormessage: "GENERIC_ERROR" });
         }
     })
 
+     /**
+     * Return
+     *The Bio informations of the user as and obj
+     *The associated courses of a user as an array of obj
+     */
     app.get('/getsingleuser/:id', authenticateToken, async (req, res) => {
-        let iduser=req.params.iduser;
+        let iduser=req.params.id;
+        console.log(iduser)
         try {
             //getting data of the user
-            const [data] = await con.execute(`select u.id as iduser,u.firstname,u.lastname,u.phone,u.age,u.email,u.status,u.fiscalcode ,u.imgurl  from users u 
+            const [user] = await con.execute(`select *  from users u 
                                             where u.id=? `,
                                             [iduser]); 
-            const [attStudent]=await con.execute(`select  m.id as moduleid,m.lenght as modulelength,m.name as modulename,SUM(TIMESTAMPDIFF(SECOND, aul.entryhour , aul.exithour)) as comulatedattendance from modules m 
-                                            inner join users_modules um on um.id_module =m.id
-                                            left join lessons l on um.id_module =l.id_modules 
-                                            left join attendance_users_lessons aul on aul.id_lesson =l.id and um.id_user =aul.id_user
-                                            where um.id_user =?  and um.permit =1 
-                                            group by um.id_module`,
-                                            [iduser]);                           
-            const [tchModules]=await con.execute(`select c.startyear ,c.endyear , c.name as coursename,m.name as modulename,m.id as moduleid from modules m 
-                                                inner join users_modules um on um.id_module =m.id
-                                                inner join courses c on c.id=m.id_course
-                                                where um.id_user =? and um.permit =2`,
-                                                [iduser]);
-            const [asCourses]=await con.execute(`select c.startyear,c.id as courseid  ,c.endyear , c.name as coursename, r.name as rolename,r.id as idrole from users u 
-                                     inner join users_roles_courses urc on urc.id_user =u.id
-                                     inner join roles r  on r.id=urc.id_role
-                                     inner join courses c on urc.id_course =c.id
-                                     where u.id=?`,
-                                     [iduser]);
-            res.json({userdata:data[0],teachedModules:tchModules,associateCourses:asCourses,studentAttendance:attStudent});
+            const [associatedcourses]=await con.execute(`select c.name,c.startyear,c.endyear,r.name 
+                                            from users u 
+                                            inner join users_roles_courses urc on urc.id_user =u.id
+                                            inner join roles r on r.id =urc.id_role
+                                            inner join courses c on c.id=urc.id_course 
+                                            where u.id=?`,
+                                            [iduser]);
+            if(user.length<=0) {
+            res.json({erros:true,errosmessage:"INVALID USER"});
+            return
+            } ;                           
+            res.json({error: false,user:user,associatedcourses:associatedcourses});
         } catch (err) {
             console.log("Getuser Error:" + err);
             res.json({ error: true, errormessage: "GENERIC_ERROR" });
         }
     })
      
-    //DOVREBBE ESSERE GIUSTA,DA PROVARE 
-    //get self and only self
+
+     /**
+     * Return
+     *The Bio informations of the user as and obj
+     */
        app.get('/getadminself/:id', authenticateToken, async (req, res) => {
-        let iduser=req.user.userid;
+        let iduser=req.params.id;
+        console.log(iduser)
         try {
             //getting data of the user
-            const [data] = await con.execute(`select u.id as iduser,u.firstname,u.lastname,u.phone,u.age,u.email,u.status,u.fiscalcode ,u.imgurl,r.id as idrole,r.name as rolename  from users u 
-                                            inner join users_roles_courses urc on urc.id_user =u.id
-                                            inner join roles r on urc.id_role =r.id
-                                            where u.id=`,
+            const [user] = await con.execute(`select *  from users u 
+                                            where u.id=? `,
                                             [iduser]);
-            //getting the module done by  the student   
-            const [datastud]=await con.execute(`select m.name,m.lenght,um.attendance ,um.absences from modules m 
-                                             inner join users_modules um on um.id_module =m.id
-                                             where um.id_user =?`,
-                                             [iduser]);                           
-            //getting the module owned by the professor/coordinator
-            const [dataprof]=await con.execute(`select c.name as coursename,m.name as modulename from modules m 
-                                                inner join users_modules um on um.id_module =m.id
-                                                inner join courses c on c.id=m.id_course
-                                                where um.id_user =? and um.permit =2`,
-                                                [iduser]);
-            //getting the courses owned by the coordinator
-            const [datacoord]=await con.execute(`select c.name as coursename from users u 
-                                     inner join users_roles_courses urc on urc.id_user =u.id
-                                     inner join courses c on urc.id_course =c.id
-                                     where urc.id_role =3 and u.id=?`,
-                                     [iduser]);
-            res.json({userdata:data,modulesowned:dataprof,courseowned:datacoord,modulesdone:datastud});
+            if(user.length<=0){
+               res.json({error:true,errormessage:"INVALID USER"}) 
+               return
+            }  
+            res.json({error: false,userdata:user});
         } catch (err) {
             console.log("Getuser Error:" + err);
             res.json({ error: true, errormessage: "GENERIC_ERROR" });
         }
         })
 
-    //DOBREBBE FUNZIONARE,VA TESTATA
-   //create a new user inside a course
+   /**
+    * Adding a new user
+    * It's not connected to the database,just added
+    */
    app.post('/adduser/',authenticateToken ,jsonParser, async (req, res) => {
-   let idcourse = req.params.idcourse;
    let rqbody = req.body;
    try{
  
@@ -141,23 +137,10 @@ function initAdminUserRoutes(app) {
        //user creation
        const [data] = await con.execute(`insert into users (password,lastname,firstname,phone,email,status,fiscalcode,age) values (?,?,?,?,?,?,?,?)`, 
                                        [hash, rqbody.lastname, rqbody.firstname, rqbody.phone, rqbody.email, rqbody.status, rqbody.fiscalcode,rqbody.age]);
-       const inserteduser=await con.query(`select id from users where fiscalcode = "${rqbody.fiscalcode}"`);
-       const iduser=inserteduser[0]["id"];
-       //connecting the user to the course
-       const [datarolcor]=await con.execute(`INSERT INTO users_roles_courses (id_user, id_role, id_course) VALUES(?,?,?);`,
-                                                                                            [iduser,rqbody.role,idcourse])
-       //connecting the user to the modules, permit 1 for the students,permit 0 for the professors and coordinator
-       const [datamod]=await con.query(` insert into users_modules (id_user,id_module,permit)
-                                         select ? as id_user,m.id as id_module,${(rqbody.role===1)? 1:0} as permit from  modules m 
-                                         left join users_modules um on um.id_user =?
-                                         where m.id_course =?`
-                                        ,[iduser,iduser,idcourse])
-       
-       res.json(`${data} ${datarolcor}${datamod}`);
+       res.json({ error: false,data:data,message:"USER INSERTED SUCCESSFULLY"});
      }else{
        res.json({ error: true, errormessage: "FISCALCODE_EXISTS"});
      }
-     
    } catch(err) {
      console.log("Createuser Error: " + err);
      res.json({ error: true, errormessage: "GENERIC_ERROR"});
@@ -166,10 +149,12 @@ function initAdminUserRoutes(app) {
  })
 
 
-    //DOBREBBE FUNZIONARE,VA TESTATA
-    //update a the user of a course
-    app.patch('/updateuser/id', jsonParser, authenticateToken, async (req, res) => {
-        let patchid = req.params.iduser;
+   /**
+    * Udating the informations of a user
+    *the user id is required 
+    */
+    app.patch('/updateuser/:id', jsonParser, authenticateToken, async (req, res) => {
+        let patchid = req.params.id;
         let rqbody = req.body;
         try {
 
@@ -183,9 +168,9 @@ function initAdminUserRoutes(app) {
             //update user
             var hash = crypto.createHash('sha256').update(rqbody.password).digest('hex');
             //update the user
-            const data = await con.execute(`update users set lastname =? ,firstname =?, phone =?, age=?,email=? ,password=? , status =?, fiscalcode =? where id =?`,
+            const [data] = await con.execute(`update users set lastname =? ,firstname =?, phone =?, age=?,email=? ,password=? , status =?, fiscalcode =? where id =?`,
                                                 [rqbody.lastname,rqbody.firstname,rqbody.phone,rqbody.age,rqbody.email,hash,rqbody.status,rqbody.fiscalcode,patchid]);
-            res.json(data,datarole);
+            res.json({ error: false,data:data, message: "DATA SUCCESSFULLY UPDATED" });
 
         } catch (err) {
             console.log("Updateuser Error: " + err);
@@ -225,8 +210,10 @@ function initAdminUserRoutes(app) {
 
     })
 
-    //DOVREBBE ESSERE COMPLETA,DA PROVARE
-    //delete a user
+    /**
+     * deleting and existing user
+     * the id is required
+     */
     app.delete('/deleteuser/:id', authenticateToken, async (req, res) => {
         let deleteid = req.params.id;
         try {
@@ -238,14 +225,15 @@ function initAdminUserRoutes(app) {
             }
 
             //delete user
-            const data = await con.execute(`delete from users where id =?`,[deleteid]);
-            res.json({error:false,message:`user deleted`});
+            const [data] = await con.execute(`delete from users where id =?`,[deleteid]);
+            res.json({error:false,data:data,message:`UDER DELETED`});
         } catch (err) {
             console.log("Deleteuser Error: " + err);
             res.json({ error: true, errormessage: "GENERIC_ERROR" });
         }
 
     })
+    
 }
 
 module.exports = initAdminUserRoutes;
